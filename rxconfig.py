4b4d222428d1cbf435cfe79ef57ad58a902f4990
armandoxxha <armandoxx@DESKTOP-C5E55F7.>
2025-02-14 14:37:28 -0600

    CORS

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,9 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="proyecto_web"
+    app_name="proyecto_web",
+    cors_allowed_origins=[
+        "http://localhost:3000",
+        "https://armando-web.vercel.app/"
+    ]
 )
